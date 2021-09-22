58e1c8b78e283d78d9b1bd4cbc15baee6e86cdb4
Stephan Kristiansen <stephan.soreng@gmail.com>
2021-09-22 14:23:27 +0200

    small change

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace samplecontainerapp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World from " + System.Environment.MachineName);
+                    await context.Response.WriteAsync("Hello World from " + System.Environment.MachineName + "!");
                 });
             });
         }
